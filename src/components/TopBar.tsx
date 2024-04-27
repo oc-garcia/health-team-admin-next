47acb87068eb6b0eddec1940d8d830c4241315c9
oc-garcia <octaviogarcia1988@gmail.com>
2024-04-27 10:57:15 -0300

    Update TopBar.tsx to import useTheme from @mui/material/styles

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
-import { useTheme } from "@emotion/react";
+import { useTheme } from "@mui/material/styles";
 import { useMediaQuery } from "@mui/material";
 
 export default function SearchAppBar() {
